160d002b183a8b9967badae768e30fcfb9f362f3
sanalokeshreddy <sanalokeshreddy@gmail.com>
2025-06-23 19:41:27 +0530

    Fixed Cors

--- src/main/java/com/devhire/controller/CareerPathController.java
+++ src/main/java/com/devhire/controller/CareerPathController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {
+        "http://localhost:3000",
+        "https://devhire-frontend.vercel.app"
+})
 @RestController
 @RequestMapping("/api/career-path")
 public class CareerPathController {
